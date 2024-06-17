21a3b85389d5f366f0ea000968ad165dfe31bae5
Lejla <lejla.nrk@gmail.com>
2024-06-17 20:07:43 +0200

    style:genre buttons

--- js/renderCardList.js
+++ js/renderCardList.js
@@ -7,19 +7,17 @@ export function renderCardList(movies, container) {
     const cardContent = document.createElement("div");
     const cardTitle = document.createElement("h3");
     const cardInfos = document.createElement("p");
-    // const cardOverview = document.createElement("p");
 
     card.className = "card";
     card.style.backgroundImage = `url('https://image.tmdb.org/t/p/w1280${movie.poster_path}')`;
     cardContent.className = 'card-content';
     cardTitle.textContent = movie.title ?? movie.name;
     cardInfos.textContent = movie.release_date.substring(0,4);
-    // cardOverview.textContent = movie.overview;
-
+   
     container.append(card);
     card.appendChild(cardContent);
     cardContent.appendChild(cardTitle);
     cardContent.appendChild(cardInfos);
-    // cardContent.appendChild(cardOverview);
+
   });
 }
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 import { GET } from "./js/get.js";
 import { renderCardList } from "./js/renderCardList.js";
 
+
 const mainContainerEl = document.querySelector(".main-container");
+const genreListEl = document.querySelector(".genres");
 
 let page = 1;
 let results = [];
@@ -19,4 +21,26 @@ const render = async () => {
 render();
 
 
-console.log(await GET("discover/movie", 1));
+//Getting all the movie genres
+const getGenreList = async () => {
+  const genreList = await GET("genre/movie/list", 1);
+  console.log(genreList.genres);
+  
+  genreList.genres.forEach((genre) => {
+    const genreBtnEl = document.createElement('button');
+    genreBtnEl.className = 'genre';
+    genreBtnEl.textContent = genre.name;
+    genreBtnEl.id = genre.id;
+
+    genreListEl.appendChild(genreBtnEl);
+  })
+
+  };
+
+getGenreList();
+
+
+  // console.log(await GET("discover/movie", 1));
+
+// movie o series list
+// https://api.themoviedb.org/3/genre/movie/list
