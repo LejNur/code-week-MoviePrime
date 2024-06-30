7f1edf4af78de784bd8aed1d4f2610bdb86f382a
Lejla <lejla.nrk@gmail.com>
2024-06-30 19:15:16 +0200

    chore:style and  bug fix

--- js/singleMovie.js
+++ js/singleMovie.js
@@ -1,32 +1,51 @@
-export function singleMovie (movie, container) {
+export function singleMovie(movie, container) {
   container.innerHTML = "";
 
-  const movieContainerEl = document.createElement('div');
-  const posterEl = document.createElement('img');
-  const movieDetailsEl = document.createElement('div');
-  const titleEl = document.createElement('h2');
-  const subtitleEl = document.createElement('p');
-  const descriptionEl = document.createElement('p');
-  const taglineEl = document.createElement('p');
+  const movieContainerEl = document.createElement("div");
+  const posterEl = document.createElement("img");
+  const movieDetailsEl = document.createElement("div");
+  const titleEl = document.createElement("h2");
+  const subtitleEl = document.createElement("p");
+  const descriptionEl = document.createElement("p");
+  const taglineEl = document.createElement("p");
 
+  //Cast container
+  const castEl = document.createElement("div");
 
-
-  movieContainerEl.classList.add('movie-container', 'overlay');
-  movieContainerEl.id = 'MOVIE'
-  movieContainerEl.style.backgroundImage= `url('https://image.tmdb.org/t/p/w1280${movie.backdrop_path}')`;
-  posterEl.className = 'movie-poster';
+  movieContainerEl.classList.add("movie-container", "overlay");
+  movieContainerEl.id = "MOVIE";
+  movieContainerEl.style.backgroundImage = `url('https://image.tmdb.org/t/p/w1280${movie.backdrop_path}')`;
+  posterEl.className = "movie-poster";
   posterEl.src = `https://image.tmdb.org/t/p/w1280${movie.poster_path}`;
-  movieDetailsEl.className = 'movie-content';
-
-
+  movieDetailsEl.className = "movie-content";
 
   titleEl.textContent = movie.title;
-  subtitleEl.textContent = `Original Language: ${movie.original_language} | Duration: ${movie.runtime}minutes | Vote: ${movie.vote_average.toFixed(1)}`;
-  subtitleEl.className = 'subtitle-paragraph';
-  descriptionEl.className = 'description-paragraph';
-  taglineEl.className = 'tagline'
+  subtitleEl.textContent = `Original Language: ${
+    movie.original_language
+  } | Duration: ${movie.runtime}minutes | Vote: ${movie.vote_average.toFixed(
+    1
+  )}`;
+  subtitleEl.className = "subtitle-paragraph";
+  descriptionEl.className = "description-paragraph";
+  taglineEl.className = "tagline";
   descriptionEl.textContent = movie.overview;
   taglineEl.textContent = `"${movie.tagline}"`;
+
+  castEl.className = "cast-container";
+  const credits = movie.credits.cast.slice(0, 6);
+
+  credits.forEach((credit) => {
+    const castBox = document.createElement("div");
+    castBox.className = "cast-box";
+    const castImg = document.createElement("img");
+    castImg.src = `https://image.tmdb.org/t/p/w185${credit.profile_path}`;
+
+    const castName = document.createElement("p");
+    castName.textContent = `${credit.original_name}`;
+    castBox.appendChild(castImg);
+    castBox.appendChild(castName);
+    castEl.appendChild(castBox);
+  });
 
   movieContainerEl.appendChild(posterEl);
   movieDetailsEl.appendChild(titleEl);
@@ -34,32 +53,23 @@ export function singleMovie (movie, container) {
   movieDetailsEl.appendChild(descriptionEl);
   movieDetailsEl.appendChild(taglineEl);
   movieContainerEl.appendChild(movieDetailsEl);
-
   container.appendChild(movieContainerEl);
 
-  container.append(movieContainerEl);
-
+  container.appendChild(castEl);
 }
 
+export function videoTrailer(trailers, container) {
+  trailers.forEach((trailer) => {
+    if (trailer.type === "Trailer" && trailer.name === "Official Trailer") {
+      const iframeEl = document.createElement("iframe");
 
-export function videoTrailer (trailers, container) {
-
-  trailers.forEach(trailer => {
-    if(trailer.type === 'Trailer' && trailer.name === 'Official Trailer') {
-      const iframeEl = document.createElement('iframe');
-
-      iframeEl.className = 'video';
+      iframeEl.className = "video";
       iframeEl.src = `https://www.youtube-nocookie.com/embed/${trailer.key}`;
-      iframeEl.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+      iframeEl.allow =
+        "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
       iframeEl.allowFullscreen = true;
-      
-
 
       container.append(iframeEl);
     }
-
-  })
-
+  });
 }
-
-
--- script.js
+++ script.js
@@ -4,17 +4,14 @@ import { singleMovie } from "./js/singleMovie.js";
 import { videoTrailer } from "./js/singleMovie.js";
 import { translations } from "./js/translations.js";
 
-
-
 const mainContainerEl = document.querySelector(".main-container");
-const headerEl = document.querySelector('#header');
+const headerEl = document.querySelector("#header");
 const genreListEl = document.querySelector(".genres");
-const navbarContainerEl = document.querySelector('.navbar-container');
+const navbarContainerEl = document.querySelector(".navbar-container");
 const pageButtons = document.querySelectorAll(".page-btn");
 const sidebarMenuEl = document.querySelector(".sidebar-menu");
 const selectLanguageEl = document.querySelector("#select-language");
-const dialogEl = document.querySelector('.dialog');
-
+const dialogEl = document.querySelector(".dialog");
 
 let page = 1;
 let results = [];
@@ -22,154 +19,145 @@ let type = "movie";
 let category = "popular";
 let endpoint = `${type}/${category}`;
 let query = "";
-let language = ""; 
+let language = "";
 let movieObj;
 let favoriteMovies = JSON.parse(localStorage.getItem("favoriteMovies")) || [];
 
-
-
 //Rendering first page
-  const render = async (endpoint, query) => {
-    const response = await GET(endpoint, page, query, language);
-    results = response.results.map((movie) => ({...movie, isFavorite: favoriteMovies.find((fav) => fav.id === movie.id),
-    }));
+const render = async (endpoint, query) => {
+  const response = await GET(endpoint, page, query, language);
+  results = response.results.map((movie) => ({
+    ...movie,
+    isFavorite: favoriteMovies.find((fav) => fav.id === movie.id),
+  }));
 
+  renderCardList(results, mainContainerEl);
+};
 
-    renderCardList(results, mainContainerEl);
-  };
-
-  window.onload = () => render(endpoint, query);
-
+window.onload = () => render(endpoint, query);
 
 //Change the language of api data
-  selectLanguageEl.addEventListener("change", () => {
-    language = selectLanguageEl.value;
-    getGenreList();
-    render(endpoint, query);
-    headerEl.textContent = translations[selectLanguageEl.value].movie
-  });
-
-
+selectLanguageEl.addEventListener("change", () => {
+  language = selectLanguageEl.value;
+  getGenreList();
+  render(endpoint, query);
+  headerEl.textContent = translations[selectLanguageEl.value].movie;
+});
 
 //Handling favorite movies
 //adding to favorites
-  function favoriteMoviesHandler(movie) {
-    if (favoriteMovies.find((item) => item.id === movie.id)) return;
-    favoriteMovies.push(movie);
-    localStorage.setItem("favoriteMovies", JSON.stringify(favoriteMovies));
-  }
+function favoriteMoviesHandler(movie) {
+  if (favoriteMovies.find((item) => item.id === movie.id)) return;
+  favoriteMovies.push(movie);
+  localStorage.setItem("favoriteMovies", JSON.stringify(favoriteMovies));
+}
 
 //removing from favorites
-  function removeFavoriteMovie(movie) {
-    favoriteMovies = favoriteMovies.filter((item) => item.id !== movie.id);
-    localStorage.setItem("favoriteMovies", JSON.stringify(favoriteMovies));
-    renderCardList(favoriteMovies, mainContainerEl);
-  }
-
+function removeFavoriteMovie(movie) {
+  favoriteMovies = favoriteMovies.filter((item) => item.id !== movie.id);
+  localStorage.setItem("favoriteMovies", JSON.stringify(favoriteMovies));
+  renderCardList(favoriteMovies, mainContainerEl);
+}
 
 //Handling add/remove on button click
-  const handleButtonClick = (event) => {
+const handleButtonClick = (event) => {
+  if (event.target.textContent === "+") {
+    dialogEl.showModal();
+    dialogEl.textContent =
+      translations[selectLanguageEl.value].addedToFavorites;
 
-    if (event.target.textContent === "+") {
-      dialogEl.showModal();
-      dialogEl.textContent = translations[selectLanguageEl.value].addedToFavorites;
+    dialogEl.classList.add("slide-in-fwd-center");
 
-      dialogEl.classList.add("slide-in-fwd-center");
-      
-      setTimeout(() => {
-        dialogEl.close();
-      }, 2000);
+    setTimeout(() => {
+      dialogEl.close();
+    }, 2000);
 
-      movieObj = JSON.parse(event.target.dataset.item);
-      results = results.map((movie) => movie.id === movieObj.id ? { ...movie, isFavorite: true } : movie);
-      favoriteMoviesHandler({ ...movieObj, isFavorite: true });
-      renderCardList(results, mainContainerEl);
-    }
+    movieObj = JSON.parse(event.target.dataset.item);
+    results = results.map((movie) =>
+      movie.id === movieObj.id ? { ...movie, isFavorite: true } : movie
+    );
+    favoriteMoviesHandler({ ...movieObj, isFavorite: true });
+    renderCardList(results, mainContainerEl);
+  }
 
-    if (event.target.textContent === "-") {
-      dialogEl.showModal();
-      dialogEl.textContent = translations[selectLanguageEl.value].confirmRemoveFromFavorites;
+  if (event.target.textContent === "-") {
+    dialogEl.showModal();
+    dialogEl.textContent =
+      translations[selectLanguageEl.value].confirmRemoveFromFavorites;
 
-      const buttonContainer = document.createElement("div");
-      const buttonYes = document.createElement("button");
-      const buttonNo = document.createElement("button");
+    const buttonContainer = document.createElement("div");
+    const buttonYes = document.createElement("button");
+    const buttonNo = document.createElement("button");
 
-      buttonContainer.className = "button-container";
-      buttonYes.className = "dialog-btn-yes";
-      buttonNo.className = "dialog-btn-no";
-
-
+    buttonContainer.className = "button-container";
+    buttonYes.className = "dialog-btn-yes";
+    buttonNo.className = "dialog-btn-no";
 
     buttonYes.textContent = translations[selectLanguageEl.value].yes;
     buttonNo.textContent = translations[selectLanguageEl.value].no;
 
-      buttonContainer.appendChild(buttonYes);
-      buttonContainer.appendChild(buttonNo);
-      dialogEl.append(buttonContainer);
+    buttonContainer.appendChild(buttonYes);
+    buttonContainer.appendChild(buttonNo);
+    dialogEl.append(buttonContainer);
 
-      buttonYes.addEventListener("click", () => {
-        dialogEl.textContent = translations[selectLanguageEl.value].removedFromFavorites;
-        movieObj = JSON.parse(event.target.dataset.item);
-        results = results.map((movie) =>
-          movie.id === movieObj.id ? { ...movie, isFavorite: false } : movie
-        );
-        removeFavoriteMovie({ ...movieObj, isFavorite: false });
-        renderCardList(results, mainContainerEl);
-        setTimeout(() => {
-          dialogEl.close();
-        }, 2500);
-      });
-
-      buttonNo.addEventListener("click", () => {
+    buttonYes.addEventListener("click", () => {
+      dialogEl.textContent =
+        translations[selectLanguageEl.value].removedFromFavorites;
+      movieObj = JSON.parse(event.target.dataset.item);
+      results = results.map((movie) =>
+        movie.id === movieObj.id ? { ...movie, isFavorite: false } : movie
+      );
+      removeFavoriteMovie({ ...movieObj, isFavorite: false });
+      renderCardList(results, mainContainerEl);
+      setTimeout(() => {
         dialogEl.close();
-      });
-    }
+      }, 2500);
+    });
+
+    buttonNo.addEventListener("click", () => {
+      dialogEl.close();
+    });
   }
+};
 
-
-mainContainerEl.addEventListener('click', handleButtonClick);
-
-
+mainContainerEl.addEventListener("click", handleButtonClick);
 
 //Getting all the movie genres
 const getGenreList = async () => {
-  const genreList = await GET("genre/movie/list", 1, '',  language);
+  const genreList = await GET(`genre/${type}/list`, 1, "", language);
   genreListEl.innerHTML = "";
 
   genreList.genres.forEach((genre) => {
-    const genreBtnEl = document.createElement('button');
-    genreBtnEl.className = 'genre';
+    const genreBtnEl = document.createElement("button");
+    genreBtnEl.className = "genre";
     genreBtnEl.textContent = genre.name;
     genreBtnEl.id = genre.id;
 
     genreListEl.appendChild(genreBtnEl);
 
-
-
-// Filtering genre
-    genreBtnEl.addEventListener('click', async (event) => {
-
-      document.querySelectorAll(".genre").forEach((btn) => btn.classList.remove("active"));
+    // Filtering genre
+    genreBtnEl.addEventListener("click", async (event) => {
+      document
+        .querySelectorAll(".genre")
+        .forEach((btn) => btn.classList.remove("active"));
       genreBtnEl.classList.add("active");
       const genreID = event.target.id;
       endpoint = `discover/${type}`;
       query = `with_genres=${genreID}`;
-       render(endpoint, query);
-    //close sidebar on mobile
-       setTimeout(() => {
+      render(endpoint, query);
+      //close sidebar on mobile
+      setTimeout(() => {
         sidebarMenuEl.classList.toggle("show");
-       }, 1000)
-    })
-  })
-   
+      }, 1000);
+    });
+  });
 };
 
 getGenreList();
 
-
 //Type Filter
-navbarContainerEl.addEventListener('click', (event) => {
-  if(event.target.tagName === 'A') {
+navbarContainerEl.addEventListener("click", (event) => {
+  if (event.target.tagName === "A") {
     const links = navbarContainerEl.querySelectorAll("a");
     links.forEach((link) => link.classList.remove("active"));
     event.target.classList.add("active");
@@ -181,15 +169,12 @@ navbarContainerEl.addEventListener('click', (event) => {
     endpoint = `${type}/${category}`;
     render(endpoint, query);
     headerEl.textContent = translations[selectLanguageEl.value][typeID];
-
-
+    getGenreList();
   }
-    
-})
-
+});
 
 //Category Filter
-sidebarMenuEl.addEventListener('click', (event) => {
+sidebarMenuEl.addEventListener("click", (event) => {
   const id = event.target.id;
   switch (id) {
     case "upcoming":
@@ -200,7 +185,7 @@ sidebarMenuEl.addEventListener('click', (event) => {
       render(endpoint);
       break;
     case "home":
-      render('movie/popular', query);
+      render("movie/popular", query);
       mainContainerEl.classList.remove("favorites");
       // mainContainerEl.addEventListener("click", cardClickHandler);
       break;
@@ -211,32 +196,31 @@ sidebarMenuEl.addEventListener('click', (event) => {
     default:
       break;
   }
-    //close sidebar on mobile
-      setTimeout(() => {
-        sidebarMenuEl.classList.toggle("show");
-      }, 1000);
-})
+  //close sidebar on mobile
+  setTimeout(() => {
+    sidebarMenuEl.classList.toggle("show");
+  }, 1000);
+});
 
 //Search Movie Title
-const searchInputEl = document.querySelector('.search-input');
-const searchButtonEl = document.querySelector('.search-icon');
+const searchInputEl = document.querySelector(".search-input");
+const searchButtonEl = document.querySelector(".search-icon");
 
 //Search on button click
-searchButtonEl.addEventListener('click', async () => {
+searchButtonEl.addEventListener("click", async () => {
   let inputValue = searchInputEl.value;
   // Check if input value is empty
   if (!inputValue) {
-    return; 
+    return;
   }
   endpoint = `search/${type}`;
   query = `query=${inputValue}`;
   render(endpoint, query);
-})
-
+});
 
 // Disable search button if input is empty
-searchInputEl.addEventListener('input', () => {
-  if (searchInputEl.value.trim() === '') {
+searchInputEl.addEventListener("input", () => {
+  if (searchInputEl.value.trim() === "") {
     searchButtonEl.disabled = true;
   } else {
     searchButtonEl.disabled = false;
@@ -246,42 +230,45 @@ searchInputEl.addEventListener('input', () => {
 //Search on Enter keyup
 searchInputEl.addEventListener("keyup", (event) => {
   if (event.key === "Enter") {
-      let inputValue = searchInputEl.value;
-      // Check if input value is empty
-      if (!inputValue) {
-        return;
-      }
-      endpoint = `search/${type}`;
-      query = `query=${inputValue}`;
-      render(endpoint, query);
-  } return;
+    let inputValue = searchInputEl.value;
+    // Check if input value is empty
+    if (!inputValue) {
+      return;
+    }
+    endpoint = `search/${type}`;
+    query = `query=${inputValue}`;
+    render(endpoint, query);
+  }
+  return;
 });
-
-
 
 // Pagination buttons
 pageButtons.forEach((button) => {
-  button.addEventListener('click', async () => {
-    if(button.classList.contains('left')) {
-      if(page <= 1) return;
+  button.addEventListener("click", async () => {
+    if (button.classList.contains("left")) {
+      if (page <= 1) return;
       page--;
     } else {
       page++;
     }
     render(endpoint, query);
-
   });
-}); 
-
-
+});
 
 //Single movie details on card click
 const cardClickHandler = async (event) => {
   let card = event.target;
   let cardID = Number(event.target.id);
   if (card.className === "card") {
-    const result = await GET(`${type}/${cardID}`, page, query, language);
+    query = `credits`;
+    const result = await GET(
+      `${type}/${cardID}`,
+      page,
+      `append_to_response=${query}`,
+      language
+    );
     singleMovie(result, mainContainerEl);
+    console.log(result.credits.cast);
     const video = await GET(`${type}/${cardID}/videos`, page, query, language);
     videoTrailer(video.results, mainContainerEl);
   }
@@ -289,11 +276,8 @@ const cardClickHandler = async (event) => {
 
 mainContainerEl.addEventListener("click", cardClickHandler);
 
-
-
-
 //Hamburger menu
-const burgerBtn = document.querySelector('.burger-btn');
-burgerBtn.addEventListener('click', () => {
-  sidebarMenuEl.classList.toggle('show');
-})
+const burgerBtn = document.querySelector(".burger-btn");
+burgerBtn.addEventListener("click", () => {
+  sidebarMenuEl.classList.toggle("show");
+});
